316f3fd0cf925f8c0ef38b07773667043912aedf
adrian <adrian.adrianborquez@gmail.com>
2022-04-28 23:04:17 -0600

    Se incluyó al index.php principal el archivo de configuracion

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
+
+<?php include 'recursos/PHP/configuracionDelSitioWeb/conf.php' ?>
  <!DOCTYPE html>
  <html lang="es">
-
  <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,7 +13,7 @@
  <body>
    <?php  include './recursos/nav/nav.php' ?>
 
-    
+
 
  </body>
 
