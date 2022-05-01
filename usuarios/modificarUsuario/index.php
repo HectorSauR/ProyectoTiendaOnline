d7242efe47bbd7023c3a5720ef3f1000df9a15b0
adrian <adrian.adrianborquez@gmail.com>
2022-04-30 19:48:13 -0600

    Se modifico la interfaz de registrar usuario

---
+++ usuarios/modificarUsuario/index.php
@@ -0,0 +1,95 @@
+<?php include '../../recursos/PHP/configuracionDelSitioWeb/conf.php' ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="../../recursos/cssprincipal/style.css">
+  <link rel="stylesheet" href="css/master.css">
+  <title>Document</title>
+</head>
+<body>
+  <?php  include '../../recursos/nav/nav.php' ?>
+  <div class="main">
+    <h1>Gestión de usuarios</h1>
+    <div class="contenedor-buscar">
+      <label>Buscar:</label> <input type="text" name="" value=""> <button type="button">Buscar</button>
+    </div>
+    <div class="contenedor-usuarios">
+      <div class="header-nombre">
+        <p>Nombre</p>
+      </div>
+      <div class="header-usuario">
+        <p>Usuario</p>
+      </div>
+
+      <div class="header-contra">
+        <p>Contraseña</p>
+      </div>
+
+      <div class="header-correo">
+        <p>Correo</p>
+      </div>
+
+      <div class="header-nivel">
+        <p>Nivel</p>
+      </div>
+
+      <div class="header-opcion">
+        <p>Opcion</p>
+      </div>
+
+      <div>
+        <p>Adrian Borquez Parra</p>
+      </div>
+
+      <div>
+        <p>Adrian1</p>
+      </div>
+
+      <div>
+        <p>Adrian1</p>
+      </div>
+
+      <div>
+        <p>adrian.adrianborquez@gmail.com</p>
+      </div>
+
+      <div>
+        <p>Admin</p>
+      </div>
+
+      <div>
+         <button type="butto">Editar</button>
+        <button type="butto">Eliminar</button>
+      </div>
+
+      <div>
+        <p>Adrian Borquez Parra</p>
+      </div>
+
+      <div>
+        <p>Adrian1</p>
+      </div>
+
+      <div>
+        <p>Adrian1</p>
+      </div>
+
+      <div>
+        <p>adrian.adrianborquez@gmail.com</p>
+      </div>
+
+      <div>
+        <p>Admin</p>
+      </div>
+
+      <div>
+         <button type="butto">Editar</button>
+        <button type="butto">Eliminar</button>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
--- usuarios/registrarUsuario/index.php
+++ usuarios/registrarUsuario/index.php
@@ -14,25 +14,42 @@
 
   <div class="main">
     <h1>Registro de usuario</h1>
-    <div class="contenedor-form">
-        <form>
-          <label>Nombre:</label>
-          <input type="text" name="" value="">
-          <label>Usuario:</label>
-          <input type="text" name="" value="">
-          <label>Contraseña:</label>
-          <input type="text" name="" value="">
-          <label>Correo:</label>
-          <input type="text" name="" value="">
-          <label>Nivel:</label>
-          <select>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-          </select>
-          <button type="submit">Registrar</button>
-        </form>
+    <div class="contenedor-app">
+
+      <div class="contenedor-form">
+          <form>
+            <label>Nombre:</label>
+            <input type="text" name="" value="">
+            <label>Usuario:</label>
+            <input type="text" name="" value="">
+            <label>Contraseña:</label>
+            <input type="text" name="" value="">
+            <label>Correo:</label>
+            <input type="text" name="" value="">
+            <label>Nivel:</label>
+            <select>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+            </select>
+            <button type="submit">Registrar</button>
+          </form>
+      </div>
+
+      <div class="contenedor-imagen">
+        <h1>Imagen</h1>
+        <div class="imagen">
+          
+        </div>
+        <button type="button" id="activarAgreagarImagen">Examinar</button>
+        <input type="file" name="" value="" name="images[]" id="agregarImagen">
+      </div>
     </div>
-  </div>
+    </div>
+
+
+  <script type="text/javascript" src="js/main.js">
+
+  </script>
 </body>
 </html>
