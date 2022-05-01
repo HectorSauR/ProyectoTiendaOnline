15fe5806fc21d388c0dc5051c6b53f19e4298ea7
ADO09 <jai_car_or11@hotmail.com>
2022-05-01 18:02:51 -0500

    aaa

--- productos/registrarProducto/index.php
+++ productos/registrarProducto/index.php
@@ -1,3 +1,5 @@
+  <?php include '../../recursos/PHP/configuracionDelSitioWeb/conf.php' ?>
+<?php include '../../recursos/PHP/metodos/verificarSesionUusuario.php' ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- recursos/PHP/configuracionDelSitioWeb/conf.php
+++ recursos/PHP/configuracionDelSitioWeb/conf.php
@@ -1,6 +1,4 @@
 <?php
 session_start();
 $pathHost = 'http://'.$_SERVER["HTTP_HOST"].'/ProyectoTiendaOnline/';
-
-
 ?>
