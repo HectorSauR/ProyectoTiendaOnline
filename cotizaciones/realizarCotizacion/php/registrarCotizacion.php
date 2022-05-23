070ba94dd33863d17127ad462ddd8a237d04d63d
BlueMadnessVG <car_arca@hotmail.com>
2022-05-22 22:28:57 -0600

    a
    a

--- cotizaciones/realizarCotizacion/php/registrarCotizacion.php
+++ cotizaciones/realizarCotizacion/php/registrarCotizacion.php
@@ -5,27 +5,7 @@
     //id de cotizacion
     $cotizacion = 1;
 
-    //CONFIRMAR EXISTENCIA DEL PRODUCTO
-    $query = "SELECT id_producto, cantidad FROM detalle_cotizacion WHERE id_cotizacion = $cotizacion;";
-    $statement = $conexion->prepare($query);
-    $statement->execute();
-    $result = $statement->fetchall();
-
-    foreach($result as $row){
-
-        $id_prod = $row["id_producto"];
-        
-        //CONSEGUIR EL STOCK DEL PRODUCTO
-        $BuscarProd = "SELECT STOCK FROM productos WHERE id_producto = $id_prod;";
-        $Execute = $conexion->query($BuscarProd);
-        $r = $Execute->fetchall(PDO::FETCH_ASSOC);
-        $nuevo_stock = $r[0]['STOCK'] - $row["cantidad"];
-
-        $agregarStock = "UPDATE `productos` SET `STOCK` = '$nuevo_stock' WHERE id_producto = $id_prod;";
-        $consulta =$conexion->prepare($agregarStock);
-        $res = $consulta->execute(); 
-    }
-
+    //MODIFICAR ESTADO DE LA COTIZACION
     $modificarCarrito = "UPDATE cotizacion SET `ID_ESTATUS` = 2 WHERE id_cotizacion = $cotizacion";
     $consulta =$conexion->prepare($modificarCarrito);
     $r = $consulta->execute();
