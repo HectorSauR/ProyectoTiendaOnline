761c6a8cc53573b7e6f1a0f5ffb965722c252ec3
ADO09 <jai_car_or11@hotmail.com>
2022-06-07 23:08:21 -0500

    aaa

--- recursos/PHP/metodos/EditarProductoBD.php
+++ recursos/PHP/metodos/EditarProductoBD.php
@@ -16,15 +16,14 @@
   
 if ($imagen_File["name"]=='' ){
 
-  $path = "../../../recursos/imagenes/productos/".$nombre;
-
-  
+  //$path = "../../../recursos/imagenes/productos/".$nombre;
 
   $sql = "UPDATE productos SET NOMBRE='$nombre', IMAGEN='$imagen', ID_CATEGORIA='$categoria',PRECIO_COMPRA='$precioc',PRECIO='$precio',STOCK='$stock',STOCK_MIN='$stockm',ID_ESTATUS='$status' WHERE ID_PRODUCTO='$id'";
   $stmt= $conexion->prepare($sql);
   $stmt->execute([$nombre, $imagen, $categoria,$precioc,$precio,$stock,$stockm,$status,$id]);
 } else
 {
+
   $fileContent = file_get_contents($imagen_File['tmp_name']);
   $path = "../../../recursos/imagenes/productos/".$nombre;
   $extension = pathinfo("../../../recursos/imagenes/productos/".$nombre."/".$imagen_File["name"], PATHINFO_EXTENSION);
