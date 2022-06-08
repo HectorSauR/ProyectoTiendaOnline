0e450bd96e795991682f678da1405f5667f5e696
adrian <adrian.adrianborquez@gmail.com>
2022-06-08 11:01:41 -0600

    bug de fecha en realizar venta arreglado

--- recursos/PHP/metodos/obtenerIdVenta.php
+++ recursos/PHP/metodos/obtenerIdVenta.php
@@ -19,9 +19,9 @@ include("../clases/conexion.php");
   }
 
   //REGISTRAR VENTA
-  $agregarVenta = "INSERT INTO venta values(?,?,?,?,?)";
+  $agregarVenta = "INSERT INTO venta values(?,?,?,CURDATE(),?)";
   $consulta =$conexion->prepare($agregarVenta);
-  $arregloDatos = array($id,"1",$formaPago,"CURDATE()","1");
+  $arregloDatos = array($id,"1",$formaPago,"1");
   $res = $consulta->execute($arregloDatos);
 
   echo $id;
