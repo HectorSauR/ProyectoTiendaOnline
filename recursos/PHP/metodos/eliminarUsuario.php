e4864a9f5ff1da8dac5613f4c90dc263ff63421f
adrian <adrian.adrianborquez@gmail.com>
2022-12-06 17:08:12 -0700

    Se modifico la logica para que el sistema no se quede sin usuarios.

--- recursos/PHP/metodos/eliminarUsuario.php
+++ recursos/PHP/metodos/eliminarUsuario.php
@@ -4,14 +4,23 @@ include("../clases/conexion.php");
 $usuario = $_POST['id'];
 
 //VERIFICAR QUE NO SE QUEDE SIN USUARIOS EL SISTEMA
+/* CODIGO NO NECESARIO POR EL MOMENTO
 $cantidadUsuarios = "select count(*) from usuario where ID_ESTATUS='1'";
 $Execute = $conexion->query($cantidadUsuarios);
 
 $r = $Execute->fetchall(PDO::FETCH_ASSOC);
 
 $totalUsuarios = $r[0]['count(*)'];
+*/
 
-if($totalUsuarios == "1"){
+//VERIFICAR QUE USUARIO RECIBIDO NO SEA IGUAL AL USUARIO ACTIVO
+//GUARDADO EN VARIABLE DE SESSION
+//UN ADMIN NO SE PUEDE DAR DE BAJA EL MISMO
+session_start();
+$usuarioActivo = $_SESSION['usuario'];
+
+
+if($usuario == $usuarioActivo){
   echo "2";
 }else{
   $eliminarUsuario = "UPDATE usuario SET ID_ESTATUS='2' WHERE USUARIO=?";
