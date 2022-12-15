3c5d73e72404f082bfd099d2b9de1aac1b5c9b91
HectorSauR <78789693+HectorSauR@users.noreply.github.com>
2022-12-15 01:33:46 -0700

    solucion de conflictos

--- reportes/productos/index.php
+++ reportes/productos/index.php
@@ -1,5 +1,5 @@
 <?php
-  include '../../recursos/PHP/configuracionDelSitioWeb/conf.php';
+include '../../recursos/PHP/configuracionDelSitioWeb/conf.php';
 ?>
 <?php include '../../recursos/PHP/metodos/verificarSesionUsuario.php' ?>
 <?php require 'pmv.php' ?>
@@ -15,100 +15,116 @@
   <link rel="stylesheet" href="../../recursos/librerias/jquery/plug-in/datables/datatables.css">
   <link rel="stylesheet" href="css/master.css">
   <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="../../iconos/font-awesome-4.7.0/css/font-awesome.min.css">
 </head>
 
 <body onload="checkCookie('<?php echo $_SESSION['usuario'] ?>')">
-<script type="text/javascript" src="../../Usuarios/modificarTema/js/master.js"></script>
+  <script type="text/javascript" src="../../Usuarios/modificarTema/js/master.js"></script>
 
   <?php include '../../recursos/nav/nav.php' ?>
 
   <section class="main">
-    <div class="contenedor-consultar-productos">
-      <h1>REPORTE DE PRODUCTOS</h1>
+    <h1>REPORTE DE PRODUCTOS</h1>
+    <div class="contenedor-form">
       <div id="prin">
         <form action="index.php" method="post" name="form1">
-            
-            <ul>
-             <li>
-               <label for="periodo">Categoria:</label>
-               <select name="periodo" id="periodo" value="<?php if (isset($_POST['generar'])) {echo $_POST['periodo'];} ?>">
-                  <?php
-                 foreach ($datos as $dat) {
-                 echo '<option value="' . $dat['ID_CATEGORIA'] . '">' . $dat['NOMBRE'] . '</option>';
-                 } //end foreach
-                  ?>
-               </select>
-             </li>
-             <li>
-               <label for="msg">Estatus</label>
-               <select name="FP" id="FP">
-               <?php
-                 foreach ($datos1 as $dat) {
-                 echo '<option value="' . $dat['ID_ESTATUS'] . '">' . $dat['DESCRIPCION'] . '</option>';
-                 } //end foreach
-                  ?>
-               </select>
-             </li>
-              <li>
-                <button id="btnGenerar" type="submit" name="generar">Generar Reporte</button>
-              </li>
-            </ul> 
-            </div>
-    <div id="export">
+          <div class="contenedor-inputs">
+            <label for="periodo">Categoria:</label>
+            <select name="periodo" id="periodo" value="<?php if (isset($_POST['generar'])) {
+                                                          echo $_POST['periodo'];
+                                                        } ?>">
+              <?php
+              foreach ($datos as $dat) {
+                echo '<option value="' . $dat['ID_CATEGORIA'] . '">' . $dat['NOMBRE'] . '</option>';
+              }
+              ?>
+            </select>
+            <label for="msg">Estatus</label>
+            <select name="FP" id="FP">
+              <?php
+              foreach ($datos1 as $dat) {
+                echo '<option value="' . $dat['ID_ESTATUS'] . '">' . $dat['DESCRIPCION'] . '</option>';
+              }
+              ?>
+            </select>
+            <button id="btnGenerar" type="submit" name="generar">Generar Reporte</button>
+          </div>
+      </div>
+      <div id="export" class="contenedor-exports">
         <h2>Exportar como</h2>
-        <button type="submit" name="reportePdf" class="report" onclick= "document.form1.action = 'reportePDF.php'; document.form1.submit()"> <img src="archivo-pdf.png" height ="80" width="100" /></button>
-        <button type="submit" name="reporteExcel" class="report" onclick= "document.form1.action = 'reporteExcel.php'; document.form1.submit()"> <img src="archivo-excel.png" height ="80" width="100" /></button>
+        <div class="contenedor-iconos">
+          <a type="button" href="#" target="_blank" name="reportePdf" onclick="document.form1.action = './reportePDF.php';
+                document.form1.submit()">
+            <i class="fa fa-file-pdf-o" aria-hidden="true"></i>
+          </a>
+          <a target="_blank" name="reporteExcel" onclick="document.form1.action = './reporteExcel.php';
+                document.form1.submit()">
+            <i class="fa fa-file-excel-o" aria-hidden="true"></i>
+          </a>
+        </div>
+      </div>
     </div>
     <table width="100%" border="1">
-          <tr>
-            <td><b><center>ID PRODCUTO</center></b></td>
-            <td><b><center>NOMBRE</center></b></td>
-            <td><b><center>PRECIO DE COMPRA</center></b></td>
-            <td><b><center>PRECIO DE VENTA</center></b></td>
-            <td><b><center>STOCK</center></b></td>
-            <td><b><center>CATEGORIA</center></b></td>
-          </tr>
-    </form>
-    
-    <?php
-  if (isset($_POST['generar'])) { ?>
-  <script>
-      document.querySelector('#periodo').value = "<?php echo $_POST['periodo']?>"
-      document.querySelector('#FP').value = "<?php echo $_POST['FP']?>"
-  </script>
-  <?php 
-   
-    // require '../../recursos/PHP/clases/conexion.php'
-    $conexion2  = new Conexion();
-    $conectar = $conexion2->getConectionMysql();
-    $categoria = $_REQUEST['periodo'];
-    $estatus = $_REQUEST['FP'];
-    //$BuscarUsuario = "select * from usuario where USUARIO = '$usuario' and CONTRA = '$pass'";
-    $resultado = mysqli_query($conectar,"SELECT prod.ID_PRODUCTO, prod.NOMBRE, prod.PRECIO_COMPRA, prod.PRECIO, prod.STOCK,
+      <tr>
+        <td><b>
+            <center>ID PRODCUTO</center>
+          </b></td>
+        <td><b>
+            <center>NOMBRE</center>
+          </b></td>
+        <td><b>
+            <center>PRECIO DE COMPRA</center>
+          </b></td>
+        <td><b>
+            <center>PRECIO DE VENTA</center>
+          </b></td>
+        <td><b>
+            <center>STOCK</center>
+          </b></td>
+        <td><b>
+            <center>CATEGORIA</center>
+          </b></td>
+      </tr>
+      </form>
+
+      <?php
+      if (isset($_POST['generar'])) { ?>
+        <script>
+          document.querySelector('#periodo').value = "<?php echo $_POST['periodo'] ?>"
+          document.querySelector('#FP').value = "<?php echo $_POST['FP'] ?>"
+        </script>
+        <?php
+
+        // require '../../recursos/PHP/clases/conexion.php'
+        $conexion2  = new Conexion();
+        $conectar = $conexion2->getConectionMysql();
+        $categoria = $_REQUEST['periodo'];
+        $estatus = $_REQUEST['FP'];
+        //$BuscarUsuario = "select * from usuario where USUARIO = '$usuario' and CONTRA = '$pass'";
+        $resultado = mysqli_query($conectar, "SELECT prod.ID_PRODUCTO, prod.NOMBRE, prod.PRECIO_COMPRA, prod.PRECIO, prod.STOCK,
                                                 cat.DESCRIPCION
                                             FROM productos prod 
                                             INNER JOIN categoria_productos cat ON prod.ID_CATEGORIA = cat.ID_CATEGORIA
                                             WHERE prod.ID_CATEGORIA = '$categoria' AND prod.ID_ESTATUS = '$estatus'");
-    
-    while($consultas = mysqli_fetch_array($resultado)){
-      echo 
-      "
+
+        while ($consultas = mysqli_fetch_array($resultado)) {
+          echo
+          "
           <tr>
-            <td>".$consultas['ID_PRODUCTO']."</td>
-            <td>".$consultas['NOMBRE']."</td>
-            <td>".$consultas['PRECIO_COMPRA']."</td>
-            <td>".$consultas['PRECIO']."</td>
-            <td>".$consultas['STOCK']."</td>
-            <td>".$consultas['DESCRIPCION']."</td>
+            <td>" . $consultas['ID_PRODUCTO'] . "</td>
+            <td>" . $consultas['NOMBRE'] . "</td>
+            <td>" . $consultas['PRECIO_COMPRA'] . "</td>
+            <td>" . $consultas['PRECIO'] . "</td>
+            <td>" . $consultas['STOCK'] . "</td>
+            <td>" . $consultas['DESCRIPCION'] . "</td>
           </tr>
         
       ";
-
-    }
-    ?>
+        }
+        ?>
     </table>
-    <?php
-  }
+  <?php
+      }
   ?>
 
 
--- reportes/ventasXFecha/index.php
+++ reportes/ventasXFecha/index.php
@@ -1,6 +1,6 @@
-<?php ob_start();?>
+<?php ob_start(); ?>
 <?php
-  include '../../recursos/PHP/configuracionDelSitioWeb/conf.php';
+include '../../recursos/PHP/configuracionDelSitioWeb/conf.php';
 ?>
 <?php include '../../recursos/PHP/metodos/verificarSesionUsuario.php' ?>
 <?php require 'pmv.php' ?>
@@ -16,85 +16,92 @@
   <link rel="stylesheet" href="../../recursos/librerias/jquery/plug-in/datables/datatables.css">
   <link rel="stylesheet" href="css/master.css">
   <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="../../iconos/font-awesome-4.7.0/css/font-awesome.min.css">
 </head>
 
 <body onload="checkCookie('<?php echo $_SESSION['usuario'] ?>')">
-<script type="text/javascript" src="../../Usuarios/modificarTema/js/master.js"></script>
+  <script type="text/javascript" src="../../Usuarios/modificarTema/js/master.js"></script>
 
   <?php include '../../recursos/nav/nav.php' ?>
 
   <section class="main">
     <div class="contenedor-consultar-productos">
       <h1>REPORTE DE VENTAS POR PERIODO DE TIEMPO</h1>
-      <div id="prin">
-        <form action="index.php" method="post" name="form1">
-            
-            <ul>
-             <li>
-               <label for="Periodo">Fecha:</label>
-               <input id="party" type="date" name="fecha">
-             </li>
-             <li>
-               <label for="msg">Forma de pago (1.- Efectivo, 2.- Tarjeta):</label>
-               <select id="FP" name="frmPago">
-               <?php
-                 foreach ($datos as $dat) {
-                 echo '<option value="' . $dat['ID_FORMA_PAGO'] . '">' . $dat['DESCRIPCION'] . '</option>';
-                 } //end foreach
-                  ?>
-               </select>
-             </li>
-             <li>
-             <label for="msg">Usuario:</label>
-                <select name="usr1" id="usr">
+      <div class="contenedor-form">
+        <div id="prin">
+          <form action="index.php" method="post" name="form1">
+            <div class="contenedor-inputs">
+              <label for="Periodo">Fecha:</label>
+              <input id="party" type="date" name="fecha">
+              <label for="msg">Forma de pago (1.- Efectivo, 2.- Tarjeta):</label>
+              <select id="FP" name="frmPago">
                 <?php
-                 foreach ($datos1 as $dat) {
-                 echo '<option value="' . $dat['ID_USUARIO'] . '">' . $dat['USUARIO'] . '</option>';
-                 } //end foreach
-                  ?>
-                </select>
-              </li>
+                foreach ($datos as $dat) {
+                  echo '<option value="' . $dat['ID_FORMA_PAGO'] . '">' . $dat['DESCRIPCION'] . '</option>';
+                } //end foreach
+                ?>
+              </select>
+              <label for="msg">Usuario:</label>
+              <select name="usr1" id="usr">
+                <?php
+                foreach ($datos1 as $dat) {
+                  echo '<option value="' . $dat['ID_USUARIO'] . '">' . $dat['USUARIO'] . '</option>';
+                } //end foreach
+                ?>
+              </select>
+              <button id="btnGenerar" name="generar" type="input">Generar Reporte</button>
+            </div>
+        </div>
+        <div id="export" class="contenedor-exports">
+          <h2>Exportar como</h2>
+          <div class="contenedor-iconos">
+            <a type="button" href="#" target="_blank" name="reportePdf" onclick= "document.form1.action = 'reportePDF.php'; document.form1.submit()">
+              <i class="fa fa-file-pdf-o" aria-hidden="true"></i>
+            </a>
+            <a target="_blank" name="reporteExcel" onclick= "document.form1.action = 'reporteExcel.php'; document.form1.submit()">
+              <i class="fa fa-file-excel-o" aria-hidden="true"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+      <table width="100%" border="1">
+        <tr>
+          <td><b>
+              <center>ID Venta</center>
+            </b></td>
+          <td><b>
+              <center>Fecha</center>
+            </b></td>
+          <td><b>
+              <center>Usuario</center>
+            </b></td>
+          <td><b>
+              <center>Forma de pago</center>
+            </b></td>
+          <td><b>
+              <center>Importe</center>
+            </b></td>
+        </tr>
+        </form>
 
-              <li>
-                <button id="btnGenerar" name="generar" type="input">Generar Reporte</button>
-              </li>
-            </ul> 
-       
+        <?php
+        if (isset($_POST['generar'])) {?>
+          <script>
+                    document.querySelector('#party').value = "<?php echo $_POST['fecha']?>"
+                    document.querySelector('#FP').value = "<?php echo $_POST['frmPago']?>"
+                    document.querySelector('#usr').value = "<?php echo $_POST['usr1']?>"
+        
+          </script>
+          <?php
 
-    </div>
-    <div id="export">
-        <h2>Exportar como</h2>
-        <button type="submit" name="reportePdf" onclick= "document.form1.action = 'reportePDF.php'; document.form1.submit()"> <img src="archivo-pdf.png" height ="80" width="100" /></button>
-        <button type="submit" name="reporteExcel" onclick= "document.form1.action = 'reporteExcel.php'; document.form1.submit()"> <img src="archivo-excel.png" height ="80" width="100" /></button>
-    </div>
-    <table width="100%" border="1">
-            <tr>
-              <td><b><center>ID Venta</center></b></td>
-              <td><b><center>Fecha</center></b></td>
-              <td><b><center>Usuario</center></b></td>
-              <td><b><center>Forma de pago</center></b></td>
-              <td><b><center>Importe</center></b></td>
-            </tr>
-    </form>
-  
-  <?php
-  if (isset($_POST['generar'])) { ?>
-  <script>
-            document.querySelector('#party').value = "<?php echo $_POST['fecha']?>"
-            document.querySelector('#FP').value = "<?php echo $_POST['frmPago']?>"
-            document.querySelector('#usr').value = "<?php echo $_POST['usr1']?>"
+          $conexion2 = new Conexion();
+          $conexion2 = $conexion2->getConectionMysql();
 
-  </script>
-  <?php
-    
-    $conexion2 = new Conexion();
-    $conexion2 = $conexion2->getConectionMysql();
-
-    $frmPago = $_REQUEST['frmPago'];
-    $usr = $_REQUEST['usr1'];
-    $fecha = $_REQUEST['fecha'];
-    //$BuscarUsuario = "select * from usuario where USUARIO = '$usuario' and CONTRA = '$pass'";
-    $resultado = mysqli_query($conexion2,"SELECT venta.FECHA, venta.ID_VENTA, venta.ID_FORMA_PAGO, venta.ID_USUARIO,
+          $frmPago = $_REQUEST['frmPago'];
+          $usr = $_REQUEST['usr1'];
+          $fecha = $_REQUEST['fecha'];
+          //$BuscarUsuario = "select * from usuario where USUARIO = '$usuario' and CONTRA = '$pass'";
+          $resultado = mysqli_query($conexion2, "SELECT venta.FECHA, venta.ID_VENTA, venta.ID_FORMA_PAGO, venta.ID_USUARIO,
                                                 detallesVenta.PRECIO,
                                                 usuario.USUARIO,
                                                 forma_pago.DESCRIPCION
@@ -103,32 +110,31 @@
                                             INNER JOIN usuario usuario ON venta.ID_USUARIO = usuario.ID_USUARIO
                                             INNER JOIN forma_pago forma_pago ON venta.ID_FORMA_PAGO = forma_pago.ID_FORMA_PAGO
                                             WHERE venta.FECHA = '$fecha' AND venta.ID_FORMA_PAGO = '$frmPago' AND venta.ID_USUARIO='$usr'");
-    
-    while($consultas = mysqli_fetch_array($resultado)){
-      echo 
-      "
+
+          while ($consultas = mysqli_fetch_array($resultado)) {
+            echo
+            "
           <tr>
-            <td>".$consultas['ID_VENTA']."</td>
-            <td>".$consultas['FECHA']."</td>
-            <td>".$consultas['USUARIO']."</td>
-            <td>".$consultas['DESCRIPCION']."</td>
-            <td>".$consultas['PRECIO']."</td>
+            <td>" . $consultas['ID_VENTA'] . "</td>
+            <td>" . $consultas['FECHA'] . "</td>
+            <td>" . $consultas['USUARIO'] . "</td>
+            <td>" . $consultas['DESCRIPCION'] . "</td>
+            <td>" . $consultas['PRECIO'] . "</td>
           </tr>
         
       ";
-
-    }
-    ?>
-    </table>
+          }
+        ?>
+      </table>
     <?php
-  }
-  ?>
+        }
+    ?>
 
 
 
   </section>
 
- 
+
 
   <script type="text/javascript" src="../../recursos/librerias/jquery/jquery-3.6.0.min.js"></script>
   <script type="text/javascript" charset="utf8" src="../../recursos/librerias/jquery/plug-in/datables/datatables.js"></script>
