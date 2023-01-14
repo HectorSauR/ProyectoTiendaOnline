888642ffb6488090a77e3d1a8e6b4427afac4dc4
adrian-parra <adrian.adrianborquez@hotmail.com>
2023-01-13 17:20:26 -0700

    Se modifico estilos y logica para registrar una nueva categoria de producto.

--- productos/registrarCategoria/index.php
+++ productos/registrarCategoria/index.php
@@ -18,34 +18,28 @@
   
   <?php include '../../recursos/nav/nav.php'?>
 
-    <div id="prin">
-
-        <h1>Registro de categorias</h1>
-        <form id="frm" action="ingresarDatosBd.php" method="post" enctype="multipart/form-data">
-            <div id="dataFF">
-            <ul>
-             <li>
-               <label for="name">Nombre:</label>
-               <input type="text" id="name" name="user_name" />
-             </li>
-             <li>
-               <label for="msg">Descripción:</label>
-               <textarea id="msg" name="user_message"></textarea>
-             </li>
-            </ul>
-            <button type ="submit" id="but">Registrar</button>
-            </div>
-                
-                  
-                
-                <div id="area">
-                      <div id="areaImagen">
-
-                      </div>
-                    <input type="file" id="btn1" name="imagen" accept="image/*"/>
+    <div class="main">
+        <h1>Registro de categoria</h1>
+        
+        <form id="formRegistroCategoria" class="contenedor-app">
+          <div class="contenedor-form">
+            <label>Nombre</label>
+            <input type="text" name="nombre" id="">
+            <label>Descripcion</label>
             
-                    <input type="button" id="btn2" value="Examinar" onclick="document.getElementById('btn1').click()">
-                </div>
+            <textarea name="descripcion" id="" cols="30" rows="10"></textarea>
+
+            <button type="submit" id="btnRegistrar">Registrar</button>
+          </div>
+
+          <div class="contenedor-imagen">
+            <h1>Imagen</h1>
+            <div class="imagen">
+    
+            </div>
+            <button type="button" id="activarAgreagarImagen">Examinar</button>
+            <input type="file" value="" name="imagen" id="agregarImagen" accept="image/*">
+          </div>
         </form>
 
     </div>
--- productos/registrarCategoria/main.js
+++ productos/registrarCategoria/main.js
@@ -1,18 +1,118 @@
-document.getElementById("btn1").onchange=function(e){
-    let reader=new FileReader();
-    reader.readAsDataURL(e.target.files[0]);
-    reader.onload=function(){
-        let preview = document.getElementById("areaImagen");
-        imagen=document.createElement('img');
-        imagen.src=reader.result;
-        imagen.style.width="200px";
-        preview.innerHTML="";
-        preview.append(imagen);
-    }
-}
+const btnRegistro = document.querySelector('#btnRegistrar')
 
-document.getElementById("btn1").addEventListener('click',
-    function(){
-        document.getElementById("areaImagen").style.padding="30px";
-    }
-)
+
+document.querySelector("#activarAgreagarImagen").addEventListener("click", () => {
+  document.querySelector("#agregarImagen").click();
+})
+
+document.querySelector("#agregarImagen").addEventListener("change", (e) => {
+ 
+
+  var imagen = e.target.files[0]
+  
+    //VISTA PREVIA DE IMAGEN
+    var imgCodificada = URL.createObjectURL(imagen)
+
+    document.querySelector(".contenedor-imagen .imagen").innerHTML =
+      `
+      <img src="${imgCodificada}">
+    `
+
+    document.querySelector(".main .contenedor-imagen .imagen img").addEventListener("click", (e) => {
+      e.target.remove();
+    })
+  
+
+ 
+})
+
+
+//LOGICA PARA PROCESAR DATOS DE FORM REGISTRO DE CATEGORIA
+document.querySelector('#formRegistroCategoria').addEventListener('submit' ,(e)=>{
+    e.preventDefault();
+    
+
+   //DATOS DEL FORMULARIO
+  var data = new FormData(e.target);
+
+  //VALIDACIONES
+
+  if(data.get('nombre') == ""){
+    Swal.fire(
+        'Campo nombre de categoria no puede quedar vacio.',
+        '',
+        'error'
+      )
+      return false
+  }
+  if(data.get('nombre').length > 20){
+    Swal.fire(
+        'Campo nombre de categoria , su longitud no puede ser mayor a 20 caracteres.',
+        '',
+        'error'
+      )
+      return false
+  }
+
+  if(data.get('descripcion') == ""){
+    Swal.fire(
+        'Campo descripcion , no puede quedar vacio.',
+        '',
+        'error'
+      )
+      return false
+  }
+
+  if(data.get('descripcion') > 20){
+    Swal.fire(
+        'Campo descripcion , su longitud no puede ser mayor a 20 caracteres.',
+        '',
+        'error'
+      )
+      return false
+  }
+
+  if (data.get("imagen").name==""){
+    Swal.fire(
+      'Tienes que elegir una imagen para la categoria',
+      '',
+      'error'
+    )
+   return false
+  }
+
+
+  //TODO CORRECTO CON VALIDACIONES
+
+  btnRegistro.classList.toggle('deshabilitarBtnRegistro')
+
+  //REGISTRAR CATEGORIA
+  fetch("../../recursos/PHP/metodos/registrarCategoriaBD.php" ,{
+      method:'POST',
+      body:data
+  }).then(r => r.json()).then(data =>{
+      btnRegistro.classList.toggle('deshabilitarBtnRegistro')
+      if(data.estado == 'error'){
+        Swal.fire(
+            '' + data.mensaje ,
+            '' ,
+            'error'
+        )
+      }else if(data.estado == 'success'){
+        Swal.fire(
+            '' + data.mensaje ,
+            '' ,
+            'success'
+        )
+        e.target.reset()
+      }
+      
+  })
+
+
+
+
+
+
+
+})
